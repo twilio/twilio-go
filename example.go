dad69d644c7b19cede66a173c6c7b1252a406b10
wklock <wklock@twilio.com>
2020-01-15 11:32:43 -0700

    fix: don't panic on error

--- example.go
+++ example.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-	"./src/rest"
-	"./src/rest/chat/v2"
 	"fmt"
+	"log"
+
+	twilio "./src/rest"
+	"./src/rest/chat/v2"
 )
 
 func main() {
 	twilioClient := twilio.NewClient("ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "your_auth_token")
-
 	initialName := "Chat Service from Go Client"
 	fmt.Println("Creating Chat Service with name:", initialName)
 	csParams := &chat.ServiceParams{FriendlyName: initialName}
 	resp, err := twilioClient.Chat.Create(csParams)
 	if err != nil {
-		panic("Failed to create the chat service")
+		log.Fatal(err)
 	}
 	fmt.Println("Created Chat Service with name:", resp.FriendlyName, "and sid:", resp.Sid)
 
 	fmt.Println("Reading Chat Service with sid:", resp.Sid)
 	resp, err = twilioClient.Chat.Read(resp.Sid, &chat.ServiceParams{})
 	if err != nil {
-		panic("Failed to read the chat service")
+		log.Fatal(err)
 	}
 	fmt.Println("Read Chat Service and got name:", resp.FriendlyName)
 
@@ -30,14 +31,15 @@ func main() {
 	updateParams := &chat.ServiceParams{FriendlyName: newName}
 	resp, err = twilioClient.Chat.Update(resp.Sid, updateParams)
 	if err != nil {
-		panic("Failed to update the chat service")
+		log.Fatal(err)
 	}
 	fmt.Println("Updated name for Chat Service with sid:", resp.Sid, "is", resp.FriendlyName)
 
 	fmt.Println(resp.Sid, resp.FriendlyName)
 	resp, err = twilioClient.Chat.Delete(resp.Sid, &chat.ServiceParams{})
 	if err != nil {
-		panic("Failed to delete the chat service")
+		log.Fatal(err)
 	}
 	fmt.Println("Deleted chat service")
+
 }
