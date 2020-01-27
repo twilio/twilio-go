fbd86910924486ee17b1b6af65367137314e198f
Alex Buchanan <abuchanan@twilio.com>
2020-01-27 11:25:25 -0700

    fix: merge conflict with twilio.go delete

---
+++ client/api.go
@@ -0,0 +1,44 @@
+// Package twilio provides bindings for Twilio's REST APIs.
+package twilio
+
+import (
+	"net/http"
+	"reflect"
+	"time"
+
+	twilio "github.com/twilio/twilio-go/internal"
+)
+
+// Twilio provides access to Twilio services.
+type Twilio struct {
+	Chat       *Chat
+	TaskRouter *TaskRouter
+}
+
+type service interface {
+	Initialize(*twilio.Client)
+}
+
+const interval = 10
+
+// NewClient provides an initialized Twilio client.
+func NewClient(accountSid string, authToken string) *Twilio {
+	var httpClient = &http.Client{
+		Timeout: time.Second * interval,
+	}
+
+	credentials := twilio.Credentials{AccountSid: accountSid, AuthToken: authToken}
+	client := &twilio.Client{Credentials: credentials, BaseURL: "twilio.com", HTTPClient: httpClient}
+
+	twilioClient := Twilio{}
+	twilioClient.Chat = new(Chat)
+	twilioClient.TaskRouter = new(TaskRouter)
+
+	tcRef := reflect.ValueOf(twilioClient)
+	for i := 0; i < tcRef.NumField(); i++ {
+		s := tcRef.Field(i).Interface().(service)
+		s.Initialize(client)
+	}
+
+	return &twilioClient
+}
