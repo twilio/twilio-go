85919ce502fc5c47eaa4c782d77b534dfadab59d
Alex Buchanan <buchanaf@gmail.com>
2020-01-29 12:26:43 -0700

    fix: proxy params should be pointers (#17)
    * fix: proxy service types
    
    * fix: convert params to pointers
    
    * fix: url params and rename to underscores
    
    * chore: remove unneeded pointer

---
+++ type_conversion.go
@@ -0,0 +1,17 @@
+package twilio
+
+// Bool is a helper routine that allocates a new bool value
+// to store v and returns a pointer to it.
+func (*Twilio) Bool(v bool) *bool { return &v }
+
+// Int is a helper routine that allocates a new int value
+// to store v and returns a pointer to it.
+func (*Twilio) Int(v int) *int { return &v }
+
+// Int64 is a helper routine that allocates a new int64 value
+// to store v and returns a pointer to it.
+func (*Twilio) Int64(v int64) *int64 { return &v }
+
+// String is a helper routine that allocates a new string value
+// to store v and returns a pointer to it.
+func (*Twilio) String(v string) *string { return &v }
