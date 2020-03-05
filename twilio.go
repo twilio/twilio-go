4b2281f1c3e6278d56e15df4e6c2290677676ae4
William Klock <wklock@twilio.com>
2020-03-05 10:24:00 -0700

    feat: add flex flow support (#27)
    * feat: add flex flow support
    
    * chore: fix json indentation
    
    * fix: add priority field to integration struct

--- twilio.go
+++ twilio.go
@@ -20,6 +20,7 @@ type Twilio struct {
 	Studio                *StudioClient
 	AvailablePhoneNumbers *AvailablePhoneNumbersClient
 	IncomingPhoneNumbers  *IncomingPhoneNumberClient
+	FlexFlow              *FlexFlowClient
 	Sync                  *SyncClient
 	Serverless            *ServerlessClient
 }
@@ -96,6 +97,7 @@ func NewClient(accountSID string, authToken string) *Twilio {
 	c.common.client = c
 	c.AvailablePhoneNumbers = NewAvailablePhoneNumbersClient(c)
 	c.IncomingPhoneNumbers = NewIncomingPhoneNumberClient(c)
+	c.FlexFlow = NewFlexFlowClient(c)
 	c.Chat = &ChatClient{
 		Service: NewChatServiceClient(c),
 		Role:    NewChatRoleClient(c),
