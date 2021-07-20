f223b950dcadaf195926009d394d123be9a17470
RJPearson94 <RJPearson94@users.noreply.github.com>
2021-07-20 23:52:34 +0100

    test: convert http method to be upper case and add missing import
    the http methods are converted to upper case to because the http method constants i.e. MethodGet, MethodPost values are uppercase

--- client/client_test.go
+++ client/client_test.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 	"time"
 
@@ -36,7 +37,7 @@ func TestClient_SendRequestError(t *testing.T) {
 	defer mockServer.Close()
 
 	client := NewClient("user", "pass")
-	resp, err := client.SendRequest("get", mockServer.URL, nil, nil) //nolint:bodyclose
+	resp, err := client.SendRequest("GET", mockServer.URL, nil, nil) //nolint:bodyclose
 	twilioError := err.(*twilio.TwilioRestError)
 	assert.Nil(t, resp)
 	assert.Equal(t, 400, twilioError.Status)
@@ -64,7 +65,7 @@ func TestClient_SendRequestErrorWithDetails(t *testing.T) {
 	defer mockServer.Close()
 
 	client := NewClient("user", "pass")
-	resp, err := client.SendRequest("get", mockServer.URL, nil, nil) //nolint:bodyclose
+	resp, err := client.SendRequest("GET", mockServer.URL, nil, nil) //nolint:bodyclose
 	twilioError := err.(*twilio.TwilioRestError)
 	details := make(map[string]interface{})
 	details["foo"] = "bar"
@@ -85,7 +86,7 @@ func TestClient_SendRequestWithRedirect(t *testing.T) {
 	defer mockServer.Close()
 
 	client := NewClient("user", "pass")
-	resp, _ := client.SendRequest("get", mockServer.URL, nil, nil) //nolint:bodyclose
+	resp, _ := client.SendRequest("GET", mockServer.URL, nil, nil) //nolint:bodyclose
 	assert.Equal(t, 307, resp.StatusCode)
 }
 
@@ -107,7 +108,7 @@ func TestClient_SetTimeoutTimesOut(t *testing.T) {
 
 	client := NewClient("user", "pass")
 	client.SetTimeout(10 * time.Microsecond)
-	_, err := client.SendRequest("get", mockServer.URL, nil, nil) //nolint:bodyclose
+	_, err := client.SendRequest("GET", mockServer.URL, nil, nil) //nolint:bodyclose
 	assert.Error(t, err)
 }
 
@@ -128,7 +129,7 @@ func TestClient_SetTimeoutSucceeds(t *testing.T) {
 
 	client := NewClient("user", "pass")
 	client.SetTimeout(10 * time.Second)
-	resp, err := client.SendRequest("get", mockServer.URL, nil, nil) //nolint:bodyclose
+	resp, err := client.SendRequest("GET", mockServer.URL, nil, nil) //nolint:bodyclose
 	assert.NoError(t, err)
 	assert.Equal(t, 200, resp.StatusCode)
 }
@@ -152,7 +153,7 @@ func TestClient_SetTimeoutCreatesClient(t *testing.T) {
 		Credentials: twilio.NewCredentials("user", "pass"),
 	}
 	client.SetTimeout(20 * time.Second)
-	resp, err := client.SendRequest("get", mockServer.URL, nil, nil) //nolint:bodyclose
+	resp, err := client.SendRequest("GET", mockServer.URL, nil, nil) //nolint:bodyclose
 	assert.NoError(t, err)
 	assert.Equal(t, 200, resp.StatusCode)
 }
@@ -172,7 +173,7 @@ func TestClient_UnicodeResponse(t *testing.T) {
 	defer mockServer.Close()
 
 	client := NewClient("user", "pass")
-	resp, _ := client.SendRequest("get", mockServer.URL, nil, nil) //nolint:bodyclose
+	resp, _ := client.SendRequest("GET", mockServer.URL, nil, nil) //nolint:bodyclose
 	assert.Equal(t, 200, resp.StatusCode)
 	body, _ := ioutil.ReadAll(resp.Body)
 	assert.Equal(t, "{\"testing-unicode\":\"Ω≈ç√, 💩\"}\n", string(body))
