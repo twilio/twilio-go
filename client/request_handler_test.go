64b558ce4f847ef129a08254a08a46a89a89fdd2
Pritam Kadam <kpritam@thoughtworks.com>
2022-03-12 17:19:06 +0530

    add test for invalid url

--- client/request_handler_test.go
+++ client/request_handler_test.go
@@ -1,6 +1,8 @@
 package client_test
 
 import (
+	"errors"
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -46,6 +48,16 @@ func TestRequestHandler_BuildUrlSetEdgeRegion(t *testing.T) {
 func TestRequestHandler_BuildHostRawHostWithoutPeriods(t *testing.T) {
 	requestHandler := NewRequestHandler("user", "pass")
 	assert.Equal(t, "https://prism_twilio:4010", assertAndGetURL(t, requestHandler, "https://prism_twilio:4010"))
+}
+
+func TestRequestHandler_BuildUrlInvalidCTLCharacter(t *testing.T) {
+	requestHandler := NewRequestHandler("user", "pass")
+	rawURL := "https://api.twilio.com/ServiceId\n"
+	parsedURL, err := requestHandler.BuildUrl(rawURL)
+	
+	expectedErr := url.Error{Op: "parse", URL: rawURL, Err: errors.New("net/url: invalid control character in URL")}
+	assert.Equal(t, &expectedErr, err)
+	assert.Equal(t, parsedURL, "")
 }
 
 func assertAndGetURL(t *testing.T, requestHandler *client.RequestHandler, rawURL string) string {
