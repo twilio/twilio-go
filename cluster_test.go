9d76ad12bc90521369b49de92cccfd270897ca34
Sam Harrison <sharrison@twilio.com>
2022-04-12 16:33:34 -0500

    fix: update type on broken cluster test

--- cluster_test.go
+++ cluster_test.go
@@ -1,5 +1,4 @@
 //go:build cluster
-// +build cluster
 
 package twilio
 
@@ -97,11 +96,11 @@ func TestListParams(t *testing.T) {
 	assert.Nil(t, err)
 	assert.NotNil(t, sink)
 
-	types := []map[string]interface{}{
-		{
+	types := []interface{}{
+		map[string]interface{}{
 			"type": "com.twilio.messaging.message.delivered",
 		},
-		{
+		map[string]interface{}{
 			"type": "com.twilio.messaging.message.sent",
 		},
 	}
