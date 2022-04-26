16b3351dc60e7d7047dc1eee29837bdad04b7dde
childish-sambino <sharrison@twilio.com>
2022-04-26 10:43:24 -0500

    feat: add unmarshaling helper for float32 (#158)

--- rest/pricing/v2/model_pricing_v2_trunking_number_originating_call_price.go
+++ rest/pricing/v2/model_pricing_v2_trunking_number_originating_call_price.go
@@ -11,9 +11,45 @@
 
 package openapi
 
+import (
+	"encoding/json"
+
+	"github.com/twilio/twilio-go/client"
+)
+
 // PricingV2TrunkingNumberOriginatingCallPrice The OriginatingCallPrice record
 type PricingV2TrunkingNumberOriginatingCallPrice struct {
 	BasePrice    float32 `json:"base_price,omitempty"`
 	CurrentPrice float32 `json:"current_price,omitempty"`
 	NumberType   string  `json:"number_type,omitempty"`
+}
+
+func (response *PricingV2TrunkingNumberOriginatingCallPrice) UnmarshalJSON(bytes []byte) (err error) {
+	raw := struct {
+		BasePrice    interface{} `json:"base_price"`
+		CurrentPrice interface{} `json:"current_price"`
+		NumberType   string      `json:"number_type"`
+	}{}
+
+	if err = json.Unmarshal(bytes, &raw); err != nil {
+		return err
+	}
+
+	*response = PricingV2TrunkingNumberOriginatingCallPrice{
+		NumberType: raw.NumberType,
+	}
+
+	responseBasePrice, err := client.UnmarshalFloat32(&raw.BasePrice)
+	if err != nil {
+		return err
+	}
+	response.BasePrice = *responseBasePrice
+
+	responseCurrentPrice, err := client.UnmarshalFloat32(&raw.CurrentPrice)
+	if err != nil {
+		return err
+	}
+	response.CurrentPrice = *responseCurrentPrice
+
+	return
 }
