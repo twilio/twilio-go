2d04c7df1f4e22d7fdcfb56b93c43798ef345243
manisha1997 <singhmanisha.250025@gmail.com>
2025-04-02 23:47:34 +0530

    chore: add oauth test

--- cluster_test.go
+++ cluster_test.go
@@ -129,3 +129,32 @@ func TestListingAvailableNumber(t *testing.T) {
 	assert.NotNil(t, resp)
 	assert.Equal(t, 2, len(resp))
 }
+
+func TestOauth(m *testing.M) {
+	from = os.Getenv("TWILIO_FROM_NUMBER_OAUTH")
+	to = os.Getenv("TWILIO_TO_NUMBER_OAUTH")
+	var clientId = os.Getenv("TWILIO_CLIENT_ID")
+	var clientSecret = os.Getenv("TWILIO_CLIENT_SECRET")
+	var accountSid = os.Getenv("TWILIO_ACCOUNT_SID_OAUTH")
+
+	clientCredentialProvider := &ClientCredentialProvider{
+		GrantType:    "client_credentials",
+		ClientId:     clientId,
+		ClientSecret: clientSecret,
+	}
+	testClient = NewRestClientWithParams(
+		ClientParams{ClientCredentialProvider: clientCredentialProvider, AccountSid: accountSid},
+	)
+
+	params := &Api.CreateMessageParams{}
+	params.SetTo(to)
+	params.SetFrom(from)
+	params.SetBody("Hello there")
+
+	resp, err := testClient.Api.CreateMessage(params)
+	assert.Nil(t, err)
+	assert.NotNil(t, resp)
+	assert.Equal(t, "Hello there", *resp.Body)
+	assert.Equal(t, from, *resp.From)
+	assert.Equal(t, to, *resp.To)
+}
